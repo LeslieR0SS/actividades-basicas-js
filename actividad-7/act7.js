77fdb0ba28aa367b7a43c0584f8520fd4af9b82f
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2022-10-10 17:59:55 +0200

    adding an example comentted

--- actividad-7/act7.js
+++ actividad-7/act7.js
@@ -1,8 +1,7 @@
 var numero = prompt("Introduce por favor un numero entero");
 //var numero = 4.5;
-
-// 5 x 5 = 25
-//alert(numero % 1 == 0 ? "Correcto! El número es entero. Calculando su factorial...": "El numero es decimal. Por favor introduce un número entero");
+// ej: 5! =120
+//alert(numero % 1 == 0 ? "Correcto! El número es entero. Calculando su factorial...": "El numero es decimal. Por favor introduce un número entero"); -> Alert con condicional dentro
 if(numero % 1 == 0){
     let accumulator = 1
     alert("Correcto! El número es entero. Calculando su factorial...");

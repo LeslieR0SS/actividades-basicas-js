6df1ba72def5a5379b6c9b7a887e1d217d82b6d7
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2022-10-10 18:13:31 +0200

    b1 done. Ask is it's OK?***

---
+++ actividad-b1/actB1.js
@@ -0,0 +1,8 @@
+var peso = prompt("Introduce por favor tu peso");
+var percentToGet = 17;
+var pesoLuna = (percentToGet / 100) * peso;
+alert("Tu peso en la luna es el " + percentToGet + "% de tu " + peso + "Kg en la tierra. Tu peso lunar es: " + pesoLuna + "kg");
+
+
+
+// PREGUNTAR SI ESTA BIEN?¿?

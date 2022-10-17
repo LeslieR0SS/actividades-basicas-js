69aa79e5f98de47e14307c922c264118698728cb
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2022-10-17 17:07:14 +0200

    b4 done

---
+++ activitat-b4/actb4.js
@@ -0,0 +1,6 @@
+var pulgadas = parseFloat(prompt("Introduce las pulgadas"));
+var cm = pulgadas * 2.54;
+var metros = pulgadas * 0.0254;
+
+alert(pulgadas + " pulgadas. Són: " + cm + "cm");
+alert(pulgadas + " pulgadas. Són: " + metros + "m");

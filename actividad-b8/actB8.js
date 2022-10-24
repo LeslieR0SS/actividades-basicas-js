617aecb1c6fb9ae7f4a375df979d6df57401eeca
LeslieR0SS <laranibarpozo@cifpfbmoll.eu>
2022-10-24 16:04:33 +0200

    8 and 9 done

---
+++ actividad-b8/actB8.js
@@ -0,0 +1,15 @@
+var a = Math.round(prompt("Introduce un numero"))
+var b = Math.round(prompt("Introduce un numero"))
+
+
+function whoIsBigger(){
+    if (a<b){
+         alert("El numero más pequeño es: " + a);
+    }
+    else{
+        alert("El numero más pequeño es " + b);
+    }
+
+}
+
+whoIsBigger();
---
+++ actividad-b9/actB9.js
@@ -0,0 +1,11 @@
+var a = Math.round(prompt("Introduce un numero para a:"));
+var b = Math.round(prompt("Introduce un numero para b:"));
+var c = Math.round(prompt("Introduce un numero para c:"));
+
+function whoIsSmaller(){
+    numbers = [a,b,c];
+    numbers.sort()
+
+}
+
+whoIsSmaller();
